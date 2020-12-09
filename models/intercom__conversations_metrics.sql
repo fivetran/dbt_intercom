4510a9970656d4e3dfec71db84b20b18c0e5c6a7
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2020-12-08 20:54:24 -0800

    comment removal for circle

--- models/intercom__conversations_metrics.sql
+++ models/intercom__conversations_metrics.sql
@@ -16,14 +16,14 @@ final as (
     conversation_part_aggregates.count_assignments,
     conversation_part_aggregates.first_contact_reply_at,
     conversation_part_aggregates.first_assignment_at,
-    round(({{ dbt_utils.datediff("conversations_enhanced.conversation_created_at", "conversation_part_aggregates.first_assignment_at", 'second') }} /60),2) as time_to_first_assignment,        --Intercom metric. I don't think it adds much value.
+    round(({{ dbt_utils.datediff("conversations_enhanced.conversation_created_at", "conversation_part_aggregates.first_assignment_at", 'second') }} /60),2) as time_to_first_assignment,
     conversation_part_aggregates.first_admin_response_at,
     round(({{ dbt_utils.datediff("conversations_enhanced.conversation_created_at", "conversation_part_aggregates.first_admin_response_at", 'second') }} /60),2) as time_to_first_response,
     conversation_part_aggregates.first_close_at,
     round(({{ dbt_utils.datediff("conversation_part_aggregates.first_contact_reply_at", "conversation_part_aggregates.first_close_at", 'second') }} /60),2) as time_to_first_close,
     conversation_part_aggregates.first_reopen_at,
     conversation_part_aggregates.last_assignment_at,
-    round(({{ dbt_utils.datediff("conversation_part_aggregates.first_contact_reply_at", "conversation_part_aggregates.last_assignment_at", 'second') }} /60),2) as time_to_last_assignment,     --Intercom metric. I don't think it adds much value.
+    round(({{ dbt_utils.datediff("conversation_part_aggregates.first_contact_reply_at", "conversation_part_aggregates.last_assignment_at", 'second') }} /60),2) as time_to_last_assignment,
     conversation_part_aggregates.last_contact_reply_at,
     conversation_part_aggregates.last_admin_response_at,
     conversation_part_aggregates.last_reopen_at,
